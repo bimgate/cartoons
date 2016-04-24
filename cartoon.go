aefb201184ed8e502f509030ef46d84ec81f82b5
Miletic Bratislav <bimgate@gmail.com>
2016-04-24 16:19:28 +0200

    Second-2 Commit

--- cartoon.go
+++ cartoon.go
@@ -4,7 +4,6 @@ type Cartoon struct {
 	Id                 int    `json: "id"`
 	Name               string `json: "name"`
 	Number_of_Episodes int    `json: "episodes_number"`
-	Episode_id         int    `json: "episode_id"`
 }
 
 type Cartoons []Cartoon
