bf309824bce6b19591066867167f0e6eb1472400
Miletic Bratislav <bimgate@gmail.com>
2016-07-03 19:37:05 +0200

    download_4

--- handlers.go
+++ handlers.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-
+	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -34,10 +37,69 @@ func CartoonEpisodesIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 func DownloadCartoon(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("waljda skida")
-	/*vars := mux.Vars(r)
-	cartoonId := vars["cartoonId"]
-	fmt.Fprintln(w, "Cartoon show:", cartoonId) */
+	//
+	var y int = 1
+	for i := 160000; i < 170000; i++ {
+
+		rawURL := "http://cdn.ttgtmedia.com/rms/computerweekly/dt%v.png"
+		url := fmt.Sprintf(rawURL, i)
+
+		//fmt.Println(url)
+
+		resp, _ := http.Get(url)
+
+		page, _ := ioutil.ReadAll(resp.Body)
+
+		if len(page) < 400 {
+			goto next_number
+		} else {
+			fmt.Println(url) //for bolt
+
+			//bolt
+
+			file_name := strconv.Itoa(y)
+
+			y++
+			file, err := os.Create(file_name)
+
+			if err != nil {
+				fmt.Println(err)
+				panic(err)
+			}
+			defer file.Close()
+
+			check := http.Client{
+				CheckRedirect: func(r *http.Request, via []*http.Request) error {
+					r.URL.Opaque = r.URL.Path
+					return nil
+				},
+			}
+
+			resp, err := check.Get(url) // add a filter to check redirect
+
+			if err != nil {
+				fmt.Println(err)
+				panic(err)
+			}
+			defer resp.Body.Close()
+			fmt.Println(resp.Status)
+
+			size, err := io.Copy(file, resp.Body)
+
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("%s with %v bytes downloaded" /*fileName,*/, size)
+
+			//bolt
+
+		}
+	next_number:
+	}
+
+	//
+
 }
 
 func CartoonEpisodeShow(w http.ResponseWriter, r *http.Request) {
