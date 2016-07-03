4b9b26da3a24cf73e8dd4698bff9e29c0efba88a
Miletic Bratislav <bimgate@gmail.com>
2016-07-03 23:24:19 +0200

    download_8

--- handlers.go
+++ handlers.go
@@ -103,8 +103,8 @@ func DownloadCartoon(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func FileServer(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+func FileServer(http.FileServer(http.Dir("./"))) {
+	w.WriteHeader(http.StatusOK)    
 
 }
 
