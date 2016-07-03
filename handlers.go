3c1bc08421b31e8809100049f0f7161311d7f94a
Miletic Bratislav <bimgate@gmail.com>
2016-07-03 23:17:08 +0200

    download_8

--- handlers.go
+++ handlers.go
@@ -104,7 +104,7 @@ func DownloadCartoon(w http.ResponseWriter, r *http.Request) {
 }
 
 func FileServer(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(http.Dir("./"))
+	http.FileServer(http.Dir("/"))
 
 }
 
