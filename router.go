eee064482a563ed6e9af77c1c52749d09016245c
Miletic Bratislav <bimgate@gmail.com>
2016-07-03 23:08:03 +0200

    download_8

--- router.go
+++ router.go
@@ -18,7 +18,7 @@ func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
 		var handler http.Handler
-		handler = route.HandlerFunc("/", fs)
+		handler = route.HandlerFunc(fs)
 		handler = Logger(handler, route.Name)
 
 		router.
