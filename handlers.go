e68bb8afbd34135524d062a5c0d9a74f6d9fc6e4
Miletic Bratislav <bimgate@gmail.com>
2016-07-03 20:51:16 +0200

    download_7

--- handlers.go
+++ handlers.go
@@ -103,7 +103,8 @@ func DownloadCartoon(w http.ResponseWriter, r *http.Request) {
 }
 
 func FileServer(w http.ResponseWriter, r *http.Request) {
-	http.Handle(http.FileServer(http.Dir("./")))
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	http.ListenAndServe("OPENSHIFT_GO_PORT", nil)
 }
 
 func CartoonEpisodeShow(w http.ResponseWriter, r *http.Request) {
