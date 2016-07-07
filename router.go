7822ab88afb4b2439ddd47f2aacc36aef8030110
Miletic Bratislav <bimgate@gmail.com>
2016-07-07 22:27:25 +0200

    download_8

--- router.go
+++ router.go
@@ -16,7 +16,7 @@ func NewRouter() *mux.Router {
 
 	static := http.FileServer(http.Dir("static"))
 	router.Handle("/static/", http.StripPrefix("/static/", static))
-	router.PathPrefix(static)
+	router.PathPrefix("/static")
 
 	for _, route := range routes {
 		var handler http.Handler
