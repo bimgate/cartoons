c486fbd35e0f4b683200cb3ac0c170f8d75c8d72
Miletic Bratislav <bimgate@gmail.com>
2016-08-29 20:58:25 +0200

    test x6

--- cartoon.go
+++ cartoon.go
@@ -1,9 +1,11 @@
 package main
 
 type Cartoon struct {
-	Id                 int    `json: "id"`
-	Name               string `json: "name"`
-	Number_of_Episodes int    `json: "episodes_number"`
+	Id                  int    `json: "id"`
+	Name                string `json: "name"`
+	Number_of_Episodes  int    `json: "episodes_number"`
+	Dilbert_Episode_URL string `json: "episode_url"`
+	Xkcd_Episode_URL    string `json: "episode_url"`
 }
 
 type Cartoons []Cartoon
