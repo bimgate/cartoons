6f27fccf1e8f6b6de9fca3a971fd6a25314b0903
Miletic Bratislav <bimgate@gmail.com>
2016-09-04 23:01:31 +0200

    bolt_8

--- repo.go
+++ repo.go
@@ -13,33 +13,33 @@ var episodes Episodes
 
 // Give us some seed data
 func init() {
-	/*
-		/////////////////////////////////////vadi iz bazu
-		databaseName := "my-database-dilbert-name.db"
-		db := nutz.NewStorage(databaseName, 0600, nil)
-		for i := 1; i < 10; i++ {
 
-			key := (string(i))
+	/////////////////////////////////////vadi iz bazu
+	databaseName := "my-database-dilbert-name.db"
+	db := nutz.NewStorage(databaseName, 0600, nil)
+	for id {
 
-			dilbert_episode_name := db.Get("dilbert", key)
+		key := (string(i))
 
-			dilbert_episode_name_print := (string(dilbert_episode_name.Data))
+		dilbert_episode_name := db.Get("dilbert", key)
 
-			fmt.Println(dilbert_episode_name_print)
-			///////////////////////////////////vadi iz bazu
-	*/
-	RepoCreateCartoon(Cartoon{Bootstrap_URL: "http://cartoons-bimgate.rhcloud.com", Name: "Dilbert", Number_of_Episodes: 120, Episodes_URL: "http://cartoons-bimgate.rhcloud.com/static/dilbert/"})
-	RepoCreateCartoon(Cartoon{Bootstrap_URL: "http://cartoons-bimgate.rhcloud.com", Name: "xkcd", Number_of_Episodes: 100, Episodes_URL: "http://cartoons-bimgate.rhcloud.com/static/xkcd/"})
-	RepoCreateCartoon(Cartoon{Bootstrap_URL: "http://cartoons-bimgate.rhcloud.com", Name: "Snoopy"})
+		dilbert_episode_name_print := (string(dilbert_episode_name.Data))
 
-	RepoCreateCartoonEpisode(Episode{Name: dilbert_episode_name_print, Episode_URL: ("http://cartoons-bimgate.rhcloud.com/static/" + dilbert_episode_name_print), Episode_share_URL: ("http://dilbert.com/strip/" + dilbert_episode_name_print)})
-	//	RepoCreateCartoonEpisode(Episode{Name: "Episode_2", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/2"})
-	//	RepoCreateCartoonEpisode(Episode{Name: "Episode_3", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/3"})
-	//	RepoCreateCartoonEpisode(Episode{Name: "Episode_4", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/4"})
-	//	RepoCreateCartoonEpisode(Episode{Name: "Episode_5", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/5"})
-	//	RepoCreateCartoonEpisode(Episode{Name: "Episode_6", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/6"})
+		fmt.Println(dilbert_episode_name_print)
+		///////////////////////////////////vadi iz bazu
 
-	//}
+		RepoCreateCartoon(Cartoon{Bootstrap_URL: "http://cartoons-bimgate.rhcloud.com", Name: "Dilbert", Number_of_Episodes: 120, Episodes_URL: "http://cartoons-bimgate.rhcloud.com/static/dilbert/"})
+		RepoCreateCartoon(Cartoon{Bootstrap_URL: "http://cartoons-bimgate.rhcloud.com", Name: "xkcd", Number_of_Episodes: 100, Episodes_URL: "http://cartoons-bimgate.rhcloud.com/static/xkcd/"})
+		RepoCreateCartoon(Cartoon{Bootstrap_URL: "http://cartoons-bimgate.rhcloud.com", Name: "Snoopy"})
+
+		RepoCreateCartoonEpisode(Episode{Name: dilbert_episode_name_print, Episode_URL: ("http://cartoons-bimgate.rhcloud.com/static/" + dilbert_episode_name_print), Episode_share_URL: ("http://dilbert.com/strip/" + dilbert_episode_name_print)})
+		//	RepoCreateCartoonEpisode(Episode{Name: "Episode_2", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/2"})
+		//	RepoCreateCartoonEpisode(Episode{Name: "Episode_3", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/3"})
+		//	RepoCreateCartoonEpisode(Episode{Name: "Episode_4", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/4"})
+		//	RepoCreateCartoonEpisode(Episode{Name: "Episode_5", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/5"})
+		//	RepoCreateCartoonEpisode(Episode{Name: "Episode_6", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/6"})
+
+	}
 }
 
 func RepoFindCartoon(id int) Cartoon {
@@ -54,27 +54,12 @@ func RepoFindCartoon(id int) Cartoon {
 
 func RepoFindCartoonEpisode(id int) Episode {
 
-	databaseName := "my-database-dilbert-name.db"
-	db := nutz.NewStorage(databaseName, 0600, nil)
-	for i := 1; i < 10; i++ {
-
-		key := (string(i))
-
-		dilbert_episode_name := db.Get("dilbert", key)
-
-		dilbert_episode_name_print := (string(dilbert_episode_name.Data))
-
-		fmt.Println(dilbert_episode_name_print)
-		///////////////////////////////////vadi iz bazu
-		return dilbert_episode_name_print
-	}
-	/*
-		for _, e := range episodes {
-			if e.Id == id {
-				return e
-			}
+	for _, e := range episodes {
+		if e.Id == id {
+			return e
 		}
-	*/
+	}
+
 	// return empty Episode if not found
 	return Episode{}
 }
