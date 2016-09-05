3c02ae18cb3b4acbe68eaf0fbeea0630c671b17f
Miletic Bratislav <bimgate@gmail.com>
2016-09-05 22:30:12 +0200

    bolt_9

--- repo.go
+++ repo.go
@@ -30,7 +30,7 @@ func init() {
 		dilbert_episode_name_print := (string(dilbert_episode_name.Data))
 
 		fmt.Println(dilbert_episode_name_print)
-		episodes = dilbert_episode_name_print
+
 		//////////////////////////////////////////////////
 		RepoCreateCartoonEpisode(Episode{Name: dilbert_episode_name_print, Episode_URL: ("http://cartoons-bimgate.rhcloud.com/static/" + dilbert_episode_name_print), Episode_share_URL: ("http://dilbert.com/strip/" + dilbert_episode_name_print)})
 		///////////////////////////////////vadi iz bazu
