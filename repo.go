f7a9b01b80be3efcd37f207ca3b8d4ae88529923
Miletic Bratislav <bimgate@gmail.com>
2016-09-07 21:58:30 +0200

    bolt_24

--- repo.go
+++ repo.go
@@ -1,13 +1,15 @@
 package main
 
-//"fmt"
+import (
+	"fmt"
 
-//"log"
-//"strconv"
+	//"log"
+	//"strconv"
 
-//"github.com/boltdb/bolt"
+	"github.com/boltdb/bolt"
 
-//"github.com/gernest/nutz"
+	//"github.com/gernest/nutz"
+)
 
 var currentId int
 var cartoons Cartoons
@@ -25,26 +27,26 @@ func init() {
 	//Open DB
 
 	for i := 1; i < 10; i++ {
-		/*
-			// retrieve the data
-			_ = db.View(func(tx *bolt.Tx) error {
 
-				dilb_bucket := []byte("dilbert")
+		// retrieve the data
+		_ = db.View(func(tx *bolt.Tx) error {
 
-				bucket := tx.Bucket(dilb_bucket)
-				if bucket == nil {
-					return fmt.Errorf("Bucket %q not found!", bucket)
-				}
+			dilb_bucket := []byte("dilbert")
 
-				val := bucket.Get([]byte("1"))
-				fmt.Println(string(val))
+			bucket := tx.Bucket(dilb_bucket)
+			if bucket == nil {
+				return fmt.Errorf("Bucket %q not found!", bucket)
+			}
 
-				RepoCreateCartoonEpisode(Episode{Name: "vtest", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/XZY", Episode_share_URL: "SHARE_URL"})
+			val := bucket.Get([]byte("1"))
+			val_print := fmt.Println(string(val))
 
-				return nil
-			})
-		*/
-		RepoCreateCartoonEpisode(Episode{Name: "TEST-TEST", Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/XZY", Episode_share_URL: "SHARE_URL"})
+			RepoCreateCartoonEpisode(Episode{Name: val_print, Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/XZY", Episode_share_URL: "SHARE_URL"})
+
+			return nil
+		})
+
+		//	RepoCreateCartoonEpisode(Episode{Name: m, Episode_URL: "http://cartoons-bimgate.rhcloud.com/static/XZY", Episode_share_URL: "SHARE_URL"})
 
 	}
 }
